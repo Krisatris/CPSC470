d96b6f446c9b5192f41b504093aa610b49011223
csUser <stella.beemer@gmail.com>
2020-09-24 15:29:10 -0700

    started project2-2

---
+++ Project2-2/proj2-2.py
@@ -0,0 +1,10 @@
+'''
+Class: CPSC 475
+Team Member 1: Stella Beemer
+Team Member 2: None
+Submitted by: Stella Beemer
+GU Username: sbeemer2
+File name: proj2-2.py
+Project uses Zipf's law the analyze the frequency of words in a corpus
+To Execute: python3 proj2-1.py
+'''
