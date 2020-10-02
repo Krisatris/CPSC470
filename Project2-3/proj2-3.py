31b93c1d7134a37b5121323c6e15a3cecc677e41
csUser <stella.beemer@gmail.com>
2020-10-02 16:08:43 -0700

    uploading txt files

--- Project2-3/proj2-3.py
+++ Project2-3/proj2-3.py
@@ -4,8 +4,8 @@ Team Member 1: Stella Beemer
 Team Member 2: None
 Submitted by: Stella Beemer
 GU Username: sbeemer2
-File name: proj2-2.py
-Project uses Zipf's law the analyze the frequency of words in a corpus
+File name: proj2-3.py
+Project uses Zipf's law the analyze the frequency of words in a spoken-word corpus
 All four files look equally zipfian as they visually have a near identical slope
 To Execute: python3 proj2-1.py
 '''
