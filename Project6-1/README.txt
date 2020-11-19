f719f78774f38145b81af9dd892b161385f730dc
csUser <stella.beemer@gmail.com>
2020-11-18 18:34:45 -0800

    proj6-1 finished

---
+++ Project6-1/README.txt
@@ -0,0 +1,11 @@
+My chosen corpus is the novella of Brick, a 2005 film written and directed by Rian
+Johnson (Knives Out, Star Wars: The Last Jedi). This movie is a film noir set in high
+school that follows Brenden, a quiet high school student trying to solve the mystery
+of why his ex was murdered. This corpus is the novella that Johnson wrote before
+penning the screenplay, and therefore follows the plot of the movie very closely.
+
+To run: python3 proj6-1.py
+
+**Some notes, I ran out of time to fully finish this project as I was slammed with
+multiple tests, papers, and projects this week. Therefore, this program only displays
+10 n-grams' distance vectors in the command line, not printed into a CSV
--- Project6-1/proj6-1.py
+++ Project6-1/proj6-1.py
@@ -4,48 +4,115 @@ Team Member 1: Stella Beemer
 Submitted by: Stella Beemer
 GU Username: sbeemer2
 File name: proj6-1.py
-To Execute: python3 proj6-1.py
+To Execute: python3 proj6-1.py ngrams
 '''
 
-import sys
+import nlpUtils as nlp
 import nltk
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 import pandas as pd
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfVectorizer
+from scipy.spatial.distance import cdist
+from scipy.stats import zscore
+from sklearn import preprocessing
+import sys
 
-def tfidf(textList, ngrams):
-    #get ngrams
-    vectorizer = CountVectorizer(ngram_range = (ngrams, ngrams))
-    X1 = vectorizer.fit_transform(textList)
-    features = (vectorizer.get_feature_names())
+def createDeleteWords():
+    deletewords = []
+    deletewords = stopwords.words('english')
+    deletewords.append('<s>')
+    deletewords.append('</s>')
+    deletewords.append('and')
+    deletewords.append('to')
+    deletewords.append('the')
+    return deletewords
 
-    #apply tfidf
-    vectorizer = TfidfVectorizer(ngram_range = (ngrams, ngrams))
-    X2 = vectorizer.fit_transform(textList)
-    scores = (X2.toarray())
+def vocabLemmatization(data, deleteWords):
+    newData = []
+    vocab = []
+    for sentence in data:
+        newSentence = ''
+        sentence = sentence.split()
+        for word in sentence:
+            if word not in deleteWords:
+                word = nlp.lemmatize(word)
+                vocab.append(word[:-1])
+                newSentence = newSentence + word
+        newData.append(newSentence)
+    vocab = list(set(vocab))
+    return vocab, newData
 
-    sums = X2.sum(axis = 0)
-    data1 = []
-    for col, term in enumerate(features):
-        data1.append((term, sums[0, col]))
-    ranking = pd.DataFrame(data1, columns = ['term', 'rank'])
-    words = (ranking.sort_values('rank', ascending = False))
-    print("\n\nWords head: \n", words.head(20))
+def tfidf_vector(doc1, doc2):
+    doc1 = ' '.join(doc1)
+    doc2 = ' '.join(doc2)
+    vectorizer = TfidfVectorizer()
+    vectors = vectorizer.fit_transform([doc1, doc2])
+    feature_names = vectorizer.get_feature_names()
+    dense = vectors.todense()
+    denselist = dense.tolist()
+    df = pd.DataFrame(denselist, columns=feature_names)
+    return df
 
-def normalize(textList):
-    resultList = []
-    for i in textList:
-        finishedLine = i
-        finishedLine = finishedLine.replace('\n', '')
-        finishedLine = ''.join(j if ord(j) < 128 else '' for j in finishedLine)
-        if not(finishedLine.isdecimal()):
-            resultList.append(finishedLine)
-    return resultList
+def makeFinalDict(words, vocab, grams):
+    finalDict = {}
+    for word in words:
+        for i in vocab:
+            finalDict[word] = {}
+    for word in words:
+        for i in vocab:
+            finalDict[word][i] = 0
+
+    for gram in grams:
+        gram = gram.split()
+        for word in gram:
+            if word in words:
+                for i in vocab:
+                    if i in gram:
+                        finalDict[word][i] = finalDict[word][i] + 1
+    return finalDict
+
+def vectorize(words, vocab, finalDict):
+    vectorDict = []
+    for word in words:
+        vector = []
+        for i in vocab:
+            vector.append(finalDict[word][i])
+        vectorDict.append(vector)
+    return vectorDict
 
 def main():
-    ngrams = int(sys.argv[1])
-    fin = open("brick.txt", 'r')
-    lineList = fin.readlines()
-    normalized = normalize(lineList)
-    tfidfList = tfidf(normalized, ngrams)
+    data = nlp.tokenize(nlp.getData('brick.txt'))
+    doc1 = data[:int(len(data)/2)]
+    doc2 = data[int(len(data)/2):]
+    deleteWords = createDeleteWords()
+    vocab1, doc1 = vocabLemmatization(doc1, deleteWords)
+    vocab2, doc2 = vocabLemmatization(doc2, deleteWords)
+    newData = doc1 + doc2
+    vocab = set(vocab1 + vocab2)
+    df1 = tfidf_vector(doc1, doc2)
+    df1 = df1[df1 > 0]
+    df1.dropna(axis = 1, inplace = True)
+    words = df1.sum().sort_values(ascending = False).head(60).index
+    grams = nlp.makeNgram(newData, 7)
+    finalDict = makeFinalDict(words, vocab, grams)
+    vectorDict = vectorize(words, vocab, finalDict)
+    distances = cdist(vectorDict, vectorDict, metric='euclidean')
+    df = pd.DataFrame(data = distances, index = words, columns = words)
+    x = df.values
+    min_max_scaler = preprocessing.MinMaxScaler()
+    x_scaled = min_max_scaler.fit_transform(x)
+    df = pd.DataFrame(x_scaled, index = words, columns = words)
+
+    print(df['the']['pin'])
+    print(df['think']['back'])
+    print(df['said']['dode'])
+    print(df['dode']['pin'])
+    print(df['you']['think'])
+    print(df['tug']['pin'])
+    print(df['he']['said'])
+    print(df['pin']['alright'])
+    print(df['the']['like'])
+    print(df['think']['like'])
 
 main()
